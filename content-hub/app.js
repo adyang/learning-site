ca45f5abc0a8e860b5df7972742e010517b34e69
adyang <adyang2011@gmail.com>
2018-08-29 23:30:14 +0800

    Serve ui build folder as static asset in production.

--- content-hub/app.js
+++ content-hub/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../learning-site-ui/build')));
 
 app.use('/posts', postsRouter);
 
