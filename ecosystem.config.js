659940611d0308a64dde4e0c2568bf9c56a56152
adyang <adyang2011@gmail.com>
2018-09-02 01:15:20 +0800

    Use correct structure to run npm start in pm2 config.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   apps : [{
     name      : 'learning-site',
-    script    : 'npm start',
+    script    : 'npm',
+    args      : 'start',
     env: {
       NODE_ENV: 'production'
     }
